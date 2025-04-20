637c2e9a6992fae78d9fc8c4485a7c20598c3ff2
Abdullah <95abdullah99@gmail.com>
2025-04-20 14:28:49 +0530

    feat: enhance header with shopping cart, wishlist, and compare icons
    - Added Material UI dependencies for icons and badges.
    - Updated Header component to include shopping cart, wishlist, and compare icons with badges.
    - Integrated tooltips for better user experience on icon buttons.
    - Improved SearchBar component with a search button using Material UI.
    - Styled badge background color in index.css for better visibility.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GoGitCompare } from "react-icons/go";
+import { FaRegHeart } from "react-icons/fa6";
+import Tooltip from '@mui/material/Tooltip';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: 1,
+    top: 4,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
 
 const Header = () => {
   return (
     <header>
-        {/* Top stipe */}
+      {/* Top stipe */}
       <div className="top-stripe py-2 border-t-[1px] border-b-[1px] border-gray-300">
         <div className="container">
           <div className="flex items-center justify-between">
@@ -41,16 +57,69 @@ const Header = () => {
       </div>
 
       {/* Navbar */}
-
       <div className="header">
         <div className="container flex items-center justify-between">
-            <div className="col1 w-[25%]">
-                <Link to={"/"}><img src="/logo.png" className="w-2xs h-32 "/></Link>
-            </div>
-            <div className="col2 w-[45%]">
-              <SearchBar/>
-            </div>
-            <div className="col3 w-[30%]"></div>
+          <div className="col1 w-[25%]">
+            <Link to={"/"}>
+              <img src="/logo.png" className="w-2xs h-20 " />
+            </Link>
+          </div>
+
+          <div className="col2 w-[45%]">
+            <SearchBar />
+          </div>
+
+          <div className="col3 w-[30%] flex items-center">
+            <ul className="flex items-center gap-3  justify-end w-full">
+              <li className="list-none">
+                <Link
+                  to={"/login"}
+                  className="link transition text-sm font-semibold"
+                >
+                  Login
+                </Link>{" "}
+                /{" "}
+                <Link
+                  to={"/register"}
+                  className="link transition text-sm font-semibold"
+                >
+                  Register
+                </Link>
+              </li>
+
+              <li>|</li>
+
+              <li>
+              <Tooltip title="Compare">
+                <IconButton aria-label="cart">
+                  <StyledBadge badgeContent={4} color="secondary">
+                    <GoGitCompare />
+                  </StyledBadge>
+                </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+              <Tooltip title="Wishlist">
+                <IconButton aria-label="cart">
+                  <StyledBadge badgeContent={4} color="secondary">
+                  <FaRegHeart />
+                  </StyledBadge>
+                </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+              <Tooltip title="Cart">
+                <IconButton aria-label="cart">
+                  <StyledBadge badgeContent={4} color="secondary">
+                    <AiOutlineShoppingCart />
+                  </StyledBadge>
+                </IconButton>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import "../SearchBar/style.css"
+import React from "react";
+import "../SearchBar/style.css";
+import Button from "@mui/material/Button";
+import { IoSearch } from "react-icons/io5";
 
 const SearchBar = () => {
   return (
     <div className="searchBar w-[100%] h-[50px] bg-[#e5e5e5] rounded-sm relative p-2">
-      <input type="text"  placeholder="Search for products...." className="w-full h-[35px] focus:outline-none  bg-inherit p-2 "/>
+      <input
+        type="text"
+        placeholder="Search for products...."
+        className="w-full h-[35px] focus:outline-none  bg-inherit p-2 "
+      />
+      <Button className="!absolute top-[5px] right-2 z-50 !w-[35px] min-w-[35px] h-9 !rounded-full">
+        <IoSearch className="text-black text-xl" />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
